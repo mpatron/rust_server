6db63e778ed1543c909736d27bfe8bd4942c411e
PATRON Mickael <mickael_patron@hotmail.com>
2026-06-04 09:06:52 +0200

    save

--- src/librairies/schemas/pet_shop.rs
+++ src/librairies/schemas/pet_shop.rs
@@ -14,7 +14,7 @@ pub struct Address {
     pub city: String,
     pub state: Option<String>,
     #[validate(length(min = 1, max = 10))]
-    pub zipCode: String,
+    pub zip_code: String,
     pub country: Country,
 }
 
@@ -31,24 +31,24 @@ pub struct Category {
 pub struct Country {
     pub id: i32,
     #[validate(length(min = 2, max = 2))]
-    pub isoCode: String,
+    pub iso_code: String,
     #[validate(length(min = 2, max = 80))]
     pub name: String,
     #[validate(length(min = 2, max = 80))]
-    pub printableName: String,
+    pub printable_name: String,
     #[validate(length(min = 3, max = 3))]
-    pub iso3Code: String,
+    pub iso3_code: String,
     #[validate(length(min = 3, max = 3))]
-    pub numCode: String,
+    pub num_code: String,
 }
 
 #[derive(Serialize, Deserialize, Validate, Debug)]
 pub struct CreditCard {
     #[validate(length(min = 1, max = 30))]
-    pub creditCardNumber: String,
-    pub creditCardType: CreditCardType,
+    pub credit_card_number: String,
+    pub credit_card_type: CreditCardType,
     #[serde(with = "chrono::serde::ts_seconds")]
-    pub creditCardExpDate: DateTime<Utc>,
+    pub credit_card_exp_date: DateTime<Utc>,
 }
 
 #[derive(Serialize, Deserialize, Debug, ToSchema)]
@@ -87,9 +87,9 @@ pub struct Item {
     #[validate(length(min = 1, max = 3000))]
     pub description: String,
     #[validate(length(min = 1, max = 512))]
-    pub imagePath: String,
+    pub image_path: String,
     #[validate(range(min = 0.0, max = 1000.0))]
-    pub unitCost: f64,
+    pub unit_cost: f64,
     pub category: Category,
     pub product: Product,
 }
@@ -116,22 +116,22 @@ pub struct Product {
 pub struct PurchaseOrder {
     pub id: i32,
     #[serde(with = "chrono::serde::ts_seconds")]
-    pub orderDate: DateTime<Utc>,
+    pub order_date: DateTime<Utc>,
     #[validate(range(min = 0.0, max = 100000.0))]
-    pub totalWithoutVat: f64,
+    pub total_without_vat: f64,
     #[validate(range(min = 0.0, max = 100.0))]
     pub vat: f64,
     #[validate(range(min = 0.0, max = 100000.0))]
-    pub totalWithVat: f64,
+    pub total_with_vat: f64,
     #[validate(range(min = 0.0, max = 100.0))]
-    pub discountRate: f64,
+    pub discount_rate: f64,
     #[validate(range(min = 0.0, max = 100000.0))]
     pub discount: f64,
     #[validate(range(min = 0.0, max = 100000.0))]
     pub total: f64,
     pub customer: Customer,
-    pub deliveryAddress: Address,
-    pub billingAddress: Option<Address>,
-    pub creditCard: CreditCard,
-    pub orderLines: Vec<OrderLine>,
+    pub delivery_address: Address,
+    pub billing_address: Option<Address>,
+    pub credit_card: CreditCard,
+    pub order_lines: Vec<OrderLine>,
 }
